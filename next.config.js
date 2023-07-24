448b9ee34f4e39d3dbb2179332c2019aa19479db
gmarte <giancarlo.marte@gmail.com>
2023-07-24 16:43:41 -0400

    initial commit

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfig
+
+module.exports = {
+    images: {
+      remotePatterns: [
+        {          
+          hostname: 'www.pngwing.com',          
+        },
+      ],
+    },
+  }
